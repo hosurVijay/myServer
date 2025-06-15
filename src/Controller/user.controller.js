617002fb3af43af6bc318c905cf5e6115c475c0e
hosurVijay <hosurvijayendra@gmail.com>
2025-06-15 17:25:30 +0530

    added user authentication with register, login, and logout endpoints

--- src/Controller/user.controller.js
+++ src/Controller/user.controller.js
@@ -1,6 +1,5 @@
 import { asyncHandler } from "../Utilis/AsyncHandler";
 import { ApiError } from "../Utilis/ApiError.js";
-import { use, useReducer } from "react";
 import { User } from "../Models/user.models";
 import { ApiResponse } from "../Utilis/ApiResponse.js";
 
@@ -11,7 +10,7 @@ const registerUser = asyncHandler(async (req, res) => {
   );
 
   if (checkEmptyFields.includes(true)) {
-    throw new ApiError(500, "All fields are required!!");
+    throw new ApiError(400, "All fields are required!!");
   }
 
   const existedUser = await User.findOne({
@@ -19,7 +18,7 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (existedUser) {
-    throw new ApiError(500, "User already exist");
+    throw new ApiError(409, "User already exist.");
   }
 
   const user = await User.create({
@@ -33,14 +32,49 @@ const registerUser = asyncHandler(async (req, res) => {
 
   if (!createdUser) {
     throw new ApiError(
-      401,
+      500,
       "something went wrong while Registering!! \n Please try again after some time!!"
     );
   }
 
   return res
-    .status(200)
-    .json(new ApiResponse(200, "userCreated successfully!!", { user }));
+    .status(201)
+    .json(new ApiResponse(201, "userCreated successfully.", { createdUser }));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!(username || email)) {
+    throw new ApiError(400, "Username or email is required.");
+  }
+  const findUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!findUser) {
+    throw new ApiError(401, "No user with such emailID or Username found!!");
+  }
+
+  const checkPassword = await findUser.isPasswordCorrect(password);
+  if (!checkPassword) {
+    throw new ApiError(401, "Password incorrect please try again!!");
+  }
+  const foundUser = await User.findById(findUser._id).select("-password");
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, "Logged In successfully.", { foundUser }));
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  // NOTE: No JWT or session logic is implemented yet.
+  // This logout is currently handled entirely on the frontend by clearing user state.
+  // In the future, when JWT authentication is added, this endpoint will be updated
+  // to clear the authentication token (e.g., httpOnly cookie). Stay tuned!
+  res
+    .status(200)
+    .json(new ApiResponse(200, "User logged out successfully.", {}));
+});
+
+export { registerUser, loginUser, logoutUser };
